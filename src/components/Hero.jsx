23cb90b5b24422ef23a729e6630c7856111a6bd0
Sandro Tsertsvadze <sandrex233@gmail.com>
2022-10-01 13:22:05 +0400

    fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,9 +11,9 @@ const Hero = () => {
                     <p className='max-w-sm text-center font-normal text-gray-500 md:text-left'>A clean and simple interface to organize your favourite websites. Open a new
                         browser tab and see your sites load instantly. Try it for free.</p>
                     <div className="flex justify-center items-center md:justify-start space-x-3">
-                        <a href='/' className='p-3 px-5 text-xs text-white bg-[#5368DF] hover:bg-white border-2 hover:border-[#5368DF] hover:text-[#5368DF]
+                        <a href='/' className='p-3 px-5 text-xs md:text-sm text-white bg-[#5368DF] hover:bg-white border-2 hover:border-[#5368DF] hover:text-[#5368DF]
                 rounded-md baseline font-medium  transition ease-in-out'>Get it on Chrome</a>
-                        <a href='/' className='p-3 px-5 text-xs text-[#252B46] border-2 hover:border-[#252B46]
+                        <a href='/' className='p-3 px-5 text-xs md:text-sm text-[#252B46] border-2 hover:border-[#252B46]
                 rounded-md baseline shadow-md font-medium  transition ease-in-out'>Get it on Firefox</a>
                     </div>
                 </div>
