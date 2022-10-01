c8c65105282d0beadd05ca4cb112c4f11e868a33
Sandro Tsertsvadze <sandrex233@gmail.com>
2022-10-01 13:05:15 +0400

    mobileVersionFixes

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -8,7 +8,7 @@ const FAQ = ({ faq, index, toggleFAQ }) => {
                 key={index}
                 onClick={() => toggleFAQ(index)}
             >
-                <div className="faq-question hover:text-[#FA5757] cursor-pointer">
+                <div className="faq-question md:hover:text-[#FA5757] cursor-pointer">
                     {faq.question}
                 </div>
                 <div className="faq-answer tracking-normal text-base leading-loose  font-normal text-gray-500 text-left">
